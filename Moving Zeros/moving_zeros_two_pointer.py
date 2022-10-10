db0233a45734e890350c61e4931ae5cc609da1a1
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2022-10-11 00:41:22 +0300

    Create moving_zeros_two_pointer.py

---
+++ Moving Zeros/moving_zeros_two_pointer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right_bound = len(nums) - 1
+
+        while left < right_bound:
+            if nums[left] == 0:
+                nums.pop(left)
+                nums.append(0)
+                right_bound -= 1
+            else:
+                left += 1
+
+        return nums

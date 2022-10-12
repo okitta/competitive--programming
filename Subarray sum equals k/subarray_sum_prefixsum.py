150be0bb3e373dad93a0ac14a9fdd823ab89e8a4
Okitta Ongaye <88825383+okitta@users.noreply.github.com>
2022-10-12 10:38:56 +0300

    Create subarray_sum_prefixsum.py

---
+++ Subarray sum equals k/subarray_sum_prefixsum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res=0
+        curSum=0
+        prefix={0:1}
+        for n in nums:
+            curSum += n
+            diff = curSum -k
+            res += prefix.get(diff,0)
+            prefix[curSum] = prefix.get(curSum,0)+1
+        return res
